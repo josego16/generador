ff121845bc343961b23f0bf9e416d04e4c424e95
josego16 <jgomlin@g.educaand.es>
2023-10-17 10:18:18 +0200

    creando clase app para hacer un testeo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
+import modelos.Localidades;
+
 public class App {
+    public static void main(String[] args) {
+        Localidades localidades = new Localidades();
+        localidades.load("codigos-postales.csv");
+        System.out.println(localidades);
+    }
 }
