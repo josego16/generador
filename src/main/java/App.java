2042a1eeb850bcc6387b01df2b49f3a6b300d172
josego16 <jgomlin@g.educaand.es>
2023-10-17 10:59:16 +0200

    modificacion clase app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,19 @@
 import modelos.Localidades;
+import modelos.Personas;
 
 public class App {
     public static void main(String[] args) {
         Localidades localidades = new Localidades();
         localidades.load("codigos-postales.csv");
         System.out.println(localidades);
+
+        Personas per = new Personas();
+        per.load("nombre_mujer.txt",
+                "nombre_hombre.txt",
+                "apellidos.txt",
+                "codigos-postales.csv",
+                "calles.txt");
+        per.generate(5);
+        System.out.println(per.getPersonas().toString());
     }
 }
